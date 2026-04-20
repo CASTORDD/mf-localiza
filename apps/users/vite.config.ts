fc079f8f1baf7f6371d8b91d7e018d0ad781b36c
david.orjuela <castord.d@gmail.com>
2026-04-20 16:40:18 -0300

    chore: setup json-server

--- apps/users/vite.config.ts
+++ apps/users/vite.config.ts
@@ -1,28 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'users',
-      filename: 'remoteEntry.js',
+      name: "users",
+      filename: "remoteEntry.js",
       exposes: {
-        './UsersList': './src/components/UsersList',
-        './UsersApp': './src/App',
+        "./UsersList": "./src/components/UsersList",
+        "./UsersApp": "./src/App",
       },
-      shared: ['react', 'react-dom'],
+      shared: ["react", "react-dom"],
     }),
   ],
   server: {
     port: 3001,
     cors: true,
+    proxy: {
+      "/api": {
+        target: process.env.API_URL || "http://localhost:5555",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   build: {
     modulePreload: false,
-    target: 'esnext',
+    target: "esnext",
     minify: false,
     cssCodeSplit: false,
   },
-})
+});
